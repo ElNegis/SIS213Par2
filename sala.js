46cf89addfe844a0cfee0d86672d56eda8cd45a7
ElNegis <128728636+ElNegis@users.noreply.github.com>
2024-02-18 02:28:09 -0400

    Update sala.js

--- sala.js
+++ sala.js
@@ -1,70 +1,124 @@
-class Tarea {
-    constructor(text) {
-        this.text = text;
-        this.completed = false;
+const fecha = document.querySelector('#fecha')
+const lista = document.querySelector('#lista')
+const elemento = document.querySelector('#elemento')
+const input = document.querySelector('#input')
+const botonEnter = document.querySelector('#boton-enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+let LIST
+
+let id 
+
+const FECHA = new Date ()
+fecha.innerHTML = FECHA.toLocaleDateString('es-MX',{weekday: 'long', month: 'short', day:'numeric'})
+ 
+
+function agregarTarea( tarea,id,realizado,eliminado) {
+    if(eliminado) {return}  
+
+    const REALIZADO = realizado ? check : uncheck 
+
+    const LINE = realizado ? lineThrough : '' 
+
+    const elemento = `
+                        <li id="elemento">
+                        <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
+                        </li>
+                    `
+    lista.insertAdjacentHTML("beforeend",elemento)
+
+}
+
+
+function tareaRealizada(element) {
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true
+}
+
+function tareaEliminada(element){
+   // console.log(element.parentNode)
+   // console.log(element.parentNode.parentNode)
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].eliminado = true
+    console.log(LIST)
+}
+
+botonEnter.addEventListener('click', ()=> {
+    const tarea = input.value
+    if(tarea){
+        agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        localStorage.setItem('TODO',JSON.stringify(LIST))
+        id++
+        input.value = ''
     }
 
-    marcaCompletada() {
-        this.completed = true;
+})
+
+document.addEventListener('keyup', function (event) {
+    if (event.key=='Enter'){
+        const tarea = input.value
+        if(tarea) {
+            agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        localStorage.setItem('TODO',JSON.stringify(LIST))
+     
+        input.value = ''
+        id++
+        console.log(LIST)
+        }
     }
-}
+    
+})
 
-const tareas = [];
 
-function agregarTarea(tareaText) {
-    const nuevaTarea = new Task(tareaText);
-    tasks.push(nuevaTarea);
-    console.log(`Tarea "${tareaText}" agregada correctamente.`);
-}
-
-function marcaCompletada(index) {
-    if (index >= 0 && index < tareas.length) {
-        tareas[index].marcaCompletada();
-        console.log(`Tarea "${tareas[index].text}" marcada como completada.`);
-    } else {
-        console.log("Índice inválido. Introduce un índice válido.");
+lista.addEventListener('click',function(event){
+    const element = event.target 
+    const elementData = element.attributes.data.value
+    console.log(elementData)
+    
+    if(elementData == 'realizado') {
+        tareaRealizada(element)
     }
-}
-
-function eliminarTarea(index) {
-    if (index >= 0 && index < tareas.length) {
-        const eliminarTarea = tareas.splice(index, 1)[0];
-        console.log(`Tarea "${eliminarTarea.text}" eliminada correctamente.`);
-    } else {
-        console.log("Índice inválido. Introduce un índice válido.");
+    else if(elementData == 'eliminado') {
+        tareaEliminada(element)
+        console.log("elimnado")
     }
+    localStorage.setItem('TODO',JSON.stringify(LIST))
+})
+
+
+
+
+let data = localStorage.getItem('TODO')
+if(data){
+    LIST = JSON.parse(data)
+    console.log(LIST)
+    id = LIST.length
+    cargarLista(LIST)
+}else {
+    LIST = []
+    id = 0
 }
 
-function Estado() {
-    console.log("\n--- Lista de Tareas ---");
-    tareas.forEach((Tarea, index) => {
-        const status = Tarea.completed ? "Completada" : "Pendiente";
-        console.log(`${index}: ${Tarea.text} (${status})`);
-    });
+
+function cargarLista(array) {
+    array.forEach(function(item){
+        agregarTarea(item.nombre,item.id,item.realizado,item.eliminado)
+    })
 }
-
-function getUserInput(prompt) {
-    const readline = require("readline");
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    return new Promise((resolve) => {
-        rl.question(prompt, (answer) => {
-            rl.close();
-            resolve(answer);
-        });
-    });
-}
-
-async function main() {
-    const tareaText = await getUserInput("Ingresa una nueva tarea: ");
-    addTask(tareaText);
-
-    markTaskAsCompleted(0);
-    deleteTask(1);
-    displayTasks();
-}
-
-main();
